26cbce7ffa76c29d8cb91d6784cc07f519373cdc
viczan <viczan2016@gmail.com>
2022-06-29 18:09:17 -0300

    cambios de id

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 function cargarAyudaNombre() {
-    var h3=document.getElementById("h3ayuda");
+    var h3=document.getElementById("ayuda");
     h3.innerHTML="Ingrese con mayúsculas su nombre";
 }
 
 function cargarAyudaApellido() {
-    var h3=document.getElementById("h3ayuda");
+    var h3=document.getElementById("ayuda");
     h3.innerHTML="Ingrese con mayúsculas su apellido";
+}
+
+function cargarAyudaEstadoCivil() {
+    var h3=document.getElementById("ayuda");
+    h3.innerHTML="Presione el cuadro para seleccionar/deseleccionar";
 }
