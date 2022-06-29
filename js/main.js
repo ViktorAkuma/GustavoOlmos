a32eea9d98ed1c799f0b34d2ad93ce23be8520a0
viczan <viczan2016@gmail.com>
2022-06-29 17:47:32 -0300

    correcciones en js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 function cargarAyudaNombre() {
-    var h3=document.getElementById(h3ayuda);
+    var h3=document.getElementById("h3ayuda");
     h3.innerHTML="Ingrese con mayúsculas su nombre";
 }
 
 function cargarAyudaApellido() {
-    var h3=document.getElementById(h3ayuda);
+    var h3=document.getElementById("h3ayuda");
     h3.innerHTML="Ingrese con mayúsculas su apellido";
 }
